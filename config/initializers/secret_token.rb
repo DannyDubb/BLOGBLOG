8f5979c003fd5928965b1b21b2510b36e6438022
DannyDubb <DubbDanny@gmail.com>
2012-05-07 12:55:38 +0200

    BLOGBLOG

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloh::Application.config.secret_token = '5d789f4ae5e29af192f8dac61538fe819f716aa5f2625d14936d69e5c6571fa0e154b686159619958f455629ce44900520ef7a4cfc51e46e08b1de27d6c398e9'
